519aebc2dd37c9ce4484a78e27dc08d8f1ccb249
ckyeon <qsky8520@gmail.com>
2023-04-07 15:44:45 +0900

    initialize

--- src/main/java/com/sequence/todo/todo/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sequence.todo.todo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
